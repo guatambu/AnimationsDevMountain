ee0f11393fc8717b213eaae914f6b47adbccd46d
guatambu <guatambu@dundak.com>
2018-08-09 10:43:30 -0700

    add imagery and animationsto clouds and button

--- AnimationsDevMountain/AnimationsDevMountain/ViewController.swift
+++ AnimationsDevMountain/AnimationsDevMountain/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var sunImageView: UIImageView!
+    @IBOutlet weak var backCloudImageView: UIImageView!
+    @IBOutlet weak var frontCloudImageView: UIImageView!
+    @IBOutlet weak var showTheSunButtonOutlet: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animateClouds()
+        animateButton()
+        
     }
 
+    func animateClouds() {
+        self.backCloudImageView.center.x -= self.view.bounds.width
+        self.frontCloudImageView.center.x -= self.view.bounds.width
+        
+        UIView.animate(withDuration: 10.0, delay: 0.0, options:[UIViewAnimationOptions.repeat], animations: {
+            self.backCloudImageView.center.x += 2 * self.view.bounds.width
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 5.0, delay: 3.0, options:[UIViewAnimationOptions.repeat], animations: {
+            self.frontCloudImageView.center.x += 2 * self.view.bounds.width
+        }, completion: nil)
+    }
 
+    func animateButton() {
+        self.showTheSunButtonOutlet.transform = CGAffineTransform(scaleX: 0, y: 0)
+        UIView.animate(withDuration: 3.0,
+                       delay: 2.0,
+                       usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 0,
+                       options: [],
+                       animations: {
+                        self.showTheSunButtonOutlet.transform = .identity
+        },
+                       completion: nil)
+    }
 }
 
